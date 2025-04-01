a326a65540b4c8f5cfab670244f413a234789491
Nelbi Leon <nelbi.leon@gmail.com>
2025-04-01 16:18:46 +0200

    ex01 : DONE

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,67 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void): _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+    return;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+    return;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+    return;
+}
+
+Fixed::Fixed(int const constInt)
+{
+    _value = (int)(constInt * (1 << Fixed::fractinnalBits));
+    return;
+}
+
+Fixed::Fixed(float const constFloat)
+{
+    _value = (float)roundf(constFloat * (1 << Fixed::fractinnalBits));
+    return;
+}
+
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return _value;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    _value = raw;
+    return;
+}
+
+Fixed & Fixed::operator=(const Fixed & rhs)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    _value = rhs.getRawBits();
+    return *this;
+}
+
+float Fixed::toFloat( void ) const
+{
+    return (float)_value / (1 << Fixed::fractinnalBits);
+}
+
+int Fixed::toInt( void ) const
+{
+    return (int)_value / (1 << Fixed::fractinnalBits);
+}
+
+std::ostream & operator<<(std:: ostream & o, Fixed const & rhs)
+{
+    o << rhs.toFloat();
+    return o;
+}
